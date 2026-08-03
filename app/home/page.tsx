fd7d5436e4dba1e83cdc53f202f561ec6aa426ed
Kévin Mazé <kekeproject@hotmail.fr>
2026-08-03 10:56:17 +0200

    create page home

---
+++ app/home/page.tsx
@@ -0,0 +1,34 @@
+import "./home.css";
+import Social from "./Social";
+import Data from "./Data";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export default function Home() {
+    AOS.init();
+
+    return (
+        <div>
+            <section
+                className="home section"
+                id="home"
+                data-aos="fade-down"
+                data-aos-duration="1500"
+            >
+                <div className="home__container container grid">
+                    <div className="home__content grid">
+                        <Social />
+
+                        <div
+                            className="home__img"
+                            data-aos="fade-left"
+                            data-aos-duration="1500"
+                        ></div>
+
+                        <Data />
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+}
